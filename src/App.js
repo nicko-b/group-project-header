8f375a096396c50198c970bc007d97712980597f
Nick Biersner <biersusaf@gmail.com>
2020-07-29 23:28:55 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        flights: []
+    }
+    componentDidMount() {
+        fetch('http://localhost:8080/flight/all')
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({ flights: data })
+                console.log(this.state.flights)
+            })
+            .catch(console.log)
+    }
+    // [...]
 }
-
 export default App;
